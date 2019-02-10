81fd9f9b83ca5746c0b4b3f6c5d7402f595c3937
Jake Barter <midg3tjake@live.co.uk>
2019-02-10 18:36:08 +0000

    Initial Commit
    This commit just contains the basic Unity setup. Plus the things below.
    - Initial UI for Main Menu, Settings, Play Menu, & Join Screen
    - Some UI elements

---
+++ Assets/Scripts/Navigation/Menus.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Navigation {
+    public class Menus : MonoBehaviour {
+        [SerializeField] private GameObject m_MainMenu;
+        [SerializeField] private GameObject m_SettingsMenu;
+        [SerializeField] private GameObject m_PlayMenu;
+        [SerializeField] private GameObject m_JoinGame;
+
+        public void ReturnToMainMenu() {
+            m_SettingsMenu.SetActive(false);
+            m_PlayMenu.SetActive(false);
+            m_JoinGame.SetActive(false);
+            m_MainMenu.SetActive(true);
+        }
+
+        public void OpenSettingsMenu() {
+            m_PlayMenu.SetActive(false);
+            m_MainMenu.SetActive(false);
+            m_SettingsMenu.SetActive(true);
+        }
+
+        public void OpenPlayMenu() {
+            m_MainMenu.SetActive(false);
+            m_SettingsMenu.SetActive(false);
+            m_PlayMenu.SetActive(true);
+        }
+
+        public void OpenJoinGame() {
+            m_PlayMenu.SetActive(false);
+            m_JoinGame.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/JoinGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scenes {
+    public class JoinGame : MonoBehaviour {
+        public Text[] textInputs = new Text[4];
+        public int[] inputValues = new int[4];
+
+        private int m_Inputted = 0;
+
+        private void Start() {
+            foreach (Text text in textInputs) {
+                text.text = "";
+            }
+        }
+
+        public void AddNumber(int number) {
+            if (m_Inputted >= 4) return;
+            inputValues[m_Inputted] = number;
+            textInputs[m_Inputted].text = number.ToString();
+            m_Inputted++;
+        }
+
+        public void DeleteValue() {
+            if (m_Inputted <= 0) return;
+            m_Inputted--;
+            textInputs[m_Inputted].text = "";
+        }
+    }
+}
