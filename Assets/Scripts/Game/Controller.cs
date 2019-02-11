35f0ea029fe3d50c9f510a737011cffda5d2df94
Jake Barter <midg3tjake@live.co.uk>
2019-02-11 10:52:14 +0000

    Began work on initial Game Screen

---
+++ Assets/Scripts/Game/Controller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Game {
+    public class Controller : MonoBehaviour {
+        public int[] options = new int[3];
+        public string question;
+        
+        private int m_Answer;
+
+        public void NewQuestion() {
+            m_Answer = 5;
+        }
+        
+        public bool CheckAnswer(int answer) {
+            return m_Answer == answer;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/OptionChoice.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Game {
+    public class OptionChoice : MonoBehaviour {
+        public int answer;
+
+        [SerializeField] private Controller m_GameScreen;
+
+        public void CheckAnswer() {
+            m_GameScreen.CheckAnswer(answer);
+        }
+    }
+}
